f8d5f5379fecedcf91be829b4adbc395b33baaa8
“pfhenley86” <fordp86@gmail.com>
2026-03-12 17:47:08 -0400

    status model tests passed

--- BookMasterMVC/obj/Debug/net10.0/BookMasterMVC.AssemblyInfo.cs
+++ BookMasterMVC/obj/Debug/net10.0/BookMasterMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookMasterMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+277fe43e9752ed006650d431862746a4c23a07e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01a48383e30a29f5ddbe579e5be0b9d7ef0c52de")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookMasterMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookMasterMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BookMasterTests/StatusModelTests.cs
+++ BookMasterTests/StatusModelTests.cs
@@ -1,6 +1,42 @@
+using BookMasterMVC.Models;
 namespace BookMasterTests;
 
 public class StatusModelTests
 {
-    
+    // Test Status Can Be Initialized
+    [Fact]
+    public void Status_CanBeInitialized_WithValues()
+    {
+        // Arrange
+        var expectedId = 1;
+        var expectedStatusName = "Read";
+
+        // Act
+        var status = new Status
+        {
+            StatusID = expectedId,
+            StatusName = expectedStatusName
+        };
+
+        // Assert
+        Assert.Equal(expectedId, status.StatusID);
+        Assert.Equal(expectedStatusName, status.StatusName);
+    }
+
+    [Theory]
+    [InlineData(1, "Read")]
+    [InlineData(2, "Reading")]
+    public void Status_InitializationWithInlineData_ChecksProperties(int id, string statusName)
+    {
+        // Act
+        var status = new Status
+        {
+            StatusID = id,
+            StatusName = statusName
+        };
+
+        // Assert
+        Assert.Equal(id, status.StatusID);
+        Assert.Equal(statusName, status.StatusName);
+    }
 }
