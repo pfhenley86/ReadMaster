10f955d05ab704799643135e38b6ccf1b91f1021
“pfhenley86” <fordp86@gmail.com>
2026-03-12 15:02:50 -0400

    styled book search page layout

--- BookMasterMVC/obj/Debug/net10.0/BookMasterMVC.AssemblyInfo.cs
+++ BookMasterMVC/obj/Debug/net10.0/BookMasterMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookMasterMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7462cebe1007ab0481307d24e13be59bd5224ef0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94f81904021339a10371bd9e6995b602d680c0bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookMasterMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookMasterMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
