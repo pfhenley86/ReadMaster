25a8a2fb2cefb956c8c8cfbd63942521d6055baa
“pfhenley86” <fordp86@gmail.com>
2026-03-16 16:30:23 -0400

    implemented book repo methods

--- BookMasterMVC/Data/StatusRepository.cs
+++ BookMasterMVC/Data/StatusRepository.cs
@@ -1,21 +1,37 @@
+using System.Data;
 using BookMasterMVC.Models;
+using Dapper;
 
 namespace BookMasterMVC.Data;
 
 public class StatusRepository : IStatusRepository
 {
+    private readonly IDbConnection _connection;
+
+    public StatusRepository(IDbConnection connection)
+    {
+        _connection = connection;
+    }
+    
+    //Add get status for creation form
+    
+    // Create
     public void InsertStatus(Status statusToInsert)
     {
-        throw new NotImplementedException();
+        _connection.Execute("INSERT INTO status (statusName) VALUES (@statusName);", 
+            new { statusToInsert.StatusName });
     }
 
+    // Update
     public void UpdateStatus(Status status)
     {
-        throw new NotImplementedException();
+        _connection.Execute("UPDATE status SET statusName=@statusName WHERE statusID=@statusID;",
+            new { status = status.StatusName, statusID = status.StatusID });
     }
-
+    
+    // Delete
     public void DeleteStatus(Status status)
     {
-        throw new NotImplementedException();
+        _connection.Execute("DELETE FROM status WHERE statusID=@statusID;", new { statusID = status.StatusID });
     }
 }
